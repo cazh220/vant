e28f0b233b577d8ac1096bd487dd37cea3283e84
caoz <caoz@ctyi.com.cn>
2020-06-24 15:50:02 +0800

    vant

---
+++ pages/swiper/swiper.js
@@ -0,0 +1,86 @@
+// pages/swiper/swiper.js
+import Dialog from "../../components/dialog/dialog";
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  onClose(event) {
+    const {
+      position,
+      instance
+    } = event.detail;
+    switch (position) {
+      case 'right':
+        Dialog.confirm({
+          message: '确定删除吗？',
+        }).then(() => {
+          instance.close();
+        }).catch(() => {
+          //取消；关闭弹框
+          instance.close();
+        });
+        break;
+    }
+  },
+})
